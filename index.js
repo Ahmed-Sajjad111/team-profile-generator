e1cbd94ed741e02fadf392f0ee1e4e53eb8805e8
Ahmed Sajjad <uasajjad11198@outlook.com>
2021-10-22 11:39:13 -0400

    created questions for user input on engineer

--- index.js
+++ index.js
@@ -59,6 +59,61 @@ const promptManager = [
     }
 ]
 
+const promptEngineer = [
+  {
+      type: 'input',
+      name: 'engineerName',
+      message: "What is the engineer's name? (Required)",
+      validate: engineerInput => {
+          if (engineerInput) {
+            return true;
+          } else {
+            console.log("Please enter the team engineer's name!");
+            return false;
+          }
+        }
+  },
+  {
+      type: 'input',
+      name: 'engineerID',
+      message: "What is the engineer's ID? (Required)",
+      validate: idInput => {
+          if (idInput) {
+            return true;
+          } else {
+            console.log("Please enter the team engineer's ID!");
+            return false;
+          }
+        }
+  },
+  {
+      type: 'input',
+      name: 'engineerEmail',
+      message: "What is the engineer's email? (Required)",
+      validate: emailInput => {
+          if (emailInput) {
+            return true;
+          } else {
+            console.log("Please enter the team engineer's email!");
+            return false;
+          }
+        }
+  },
+  {
+      type: 'input',
+      name: 'engineerGithub',
+      message: "What is the engineer's GitHub Username? (Required)",
+      validate: githubInput => {
+          if (githubInput) {
+            return true;
+          } else {
+            console.log("Please enter the engineer's GitHub Username!");
+            return false;
+          }
+        }
+  }
+]
+
 function init() {
     inquirer.prompt(promptManager).then((managerData) => {
         
